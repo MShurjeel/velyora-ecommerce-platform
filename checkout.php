f59b19ec2d8c1f66bff02530da6b8a212cefceee
Muhammad Shurjeel Javaid <muhammadshurjeeljavaid@gmail.com>
2026-09-15 15:34:07 +0500

    Revert checkout page changes from yesterday

--- checkout.php
+++ checkout.php
@@ -27,6 +27,22 @@ $tax = $totals['tax'];
 $total = $totals['total'];
 $itemCount = $totals['item_count'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo htmlspecialchars($pageTitle); ?></title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/checkout.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+
 <?php include 'includes/header.php'; ?>
 
 <main>
